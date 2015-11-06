a26f4aa7c1395d9b8909651519b2baa40e7d4290
Jason de Carvalho <j.decarvalho@ocado.com>
2015-11-06 16:54:57 +0000

    Add new states and templates.

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -22,8 +22,50 @@ app.config([
                     return posts.get($stateParams.id);
                 }]
             }
-        });
+        }).state('login', {
+            url: '/login',
+            templateUrl: '/login.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'auth', function ($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+            }]
+        }).state('register', {
+                url: '/register',
+                templateUrl: '/register.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function ($state, auth) {
+                    if(auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }]
+            }
+        );
         $urlRouterProvider.otherwise('home');
+    }
+]);
+
+app.controller('AuthCtrl', [
+    '$scope',
+    '$state',
+    'auth',
+    function ($scope, $state, auth) {
+        $scope.user = {};
+        $scope.register = function () {
+            auth.register($scope.user).error(function (error) {
+                $scope.error = error;
+            }).then(function () {
+                $state.go('home');
+            });
+        };
+        $scope.logIn = function () {
+            $auth.logIn($scope.user).error(function (error) {
+                $scope.error = error;
+            }).then(function () {
+                $state.go('home');
+            });
+        };
     }
 ]);
 
