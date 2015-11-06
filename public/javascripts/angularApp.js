208d116541493c1025acaf2c8855f98072fb2433
Jason de Carvalho <j.decarvalho@ocado.com>
2015-11-06 16:41:33 +0000

    Create auth factory.

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -111,3 +111,46 @@ app.factory('posts', ['$http', function ($http) {
     };
     return o;
 }]);
+
+app.factory('auth', ['$http', '$window', function ($http, $window) {
+    var tokenKey = 'flapper-news-token';
+
+    var auth = {};
+    auth.saveToken = function (token) {
+        $window.localStorage[tokenKey] = token;
+    };
+    auth.getToken = function () {
+        return $window.localStorage[tokenKey];
+    };
+    auth.isLoggedIn = function () {
+        var token = auth.getToken();
+        if (token) {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.exp > Date.now() / 1000;
+        } else {
+            return false;
+        }
+    };
+    auth.currentUser = function () {
+        if (auth.isLoggedIn()) {
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.username;
+        }
+    };
+    auth.register = function (user) {
+        return $http.post('/register', user).success(function (data) {
+            auth.saveToken(data.token);
+        });
+    };
+    auth.logIn = function (user) {
+        return $http.post('/login', user).success(function (data) {
+            auth.saveToken(data.token);
+        });
+    };
+    auth.logOut = function () {
+        $window.localStorage.removeItem(tokenKey);
+    };
+
+    return auth;
+}]);
