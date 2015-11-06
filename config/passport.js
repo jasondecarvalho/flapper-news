8c3b81f9388f423878684e8c144b1118bda5be85
Jason de Carvalho <j.decarvalho@ocado.com>
2015-11-06 16:22:54 +0000

    Create passport strategy.

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+var errorMessage = 'Incorrect username or password';
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.findOne({username: username}, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, {message: errorMessage});
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, {message: errorMessage});
+            }
+            return done(null, true);
+        });
+    }
+));
