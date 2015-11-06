2f96dd918394ddfe1708e2b95047612044d428a8
Jason de Carvalho <j.decarvalho@ocado.com>
2015-11-06 14:47:46 +0000

    Add /comments resources.

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,20 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+router.param('post', function (req, res, next, id) {
+    var query = Post.findById(id);
+    query.exec(function (err, post) {
+        if (err) {
+            return next(err);
+        }
+        if (!post) {
+            return next(new Error('can\'t find post'));
+        }
+        req.post = post;
+        return next();
+    });
+});
+
 router.get('/posts', function (req, res, next) {
     Post.find(function (err, posts) {
         if (err) {
@@ -19,21 +33,12 @@ router.get('/posts', function (req, res, next) {
     });
 });
 
-router.param('post', function (req, res, next, id) {
-    var query = Post.findById(id);
-    query.exec(function (err, post) {
+router.get('/posts/:post', function (req, res) {
+    req.post.populate('comments', function (err, post) {
         if (err) {
             return next(err);
         }
-        if(!post) {
-            return next(new Error('can\'t find post'));
-        }
-        req.post = post;
-        return next();
     });
-});
-
-router.get('/posts/:post', function(req, res) {
     res.json(req.post);
 });
 
@@ -49,7 +54,48 @@ router.post('/posts', function (req, res, next) {
 
 router.post('/posts/:post/upvote', function (req, res, next) {
     req.post.upvote(function (err, post) {
-        if(err) {
+        if (err) {
+            return next(err);
+        }
+        res.json(post);
+    });
+});
+
+router.param('comment', function (req, res, next, id) {
+    var query = Comment.findById(id);
+    query.exec(function (err, comment) {
+        if (err) {
+            return next(err);
+        }
+        if (!comment) {
+            return next(new Error('can\'t find comment'));
+        }
+        req.comment = comment;
+        return next();
+    });
+});
+
+router.post('/posts/:post/comments', function (req, res, next) {
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+    comment.save(function (err, comment) {
+        if (err) {
+            return next(err);
+        }
+        req.post.comments.push(comment);
+        req.post.save(function (err, post) {
+            if (err) {
+                return next(err);
+                res.json(comment);
+            }
+        });
+    });
+    res.json(comment);
+});
+
+router.post('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+    req.comment.upvote(function (err, post) {
+        if (err) {
             return next(err);
         }
         res.json(post);
